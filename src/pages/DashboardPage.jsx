72465597ff5afd422532d0a8642505996a3d0600
dmwyzdan@gmail,com <dmwyzdan@gmail.com>
2025-06-26 16:10:54 +0700

    fix: minor UI DashboardPage

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -34,19 +34,18 @@ const DashboardPage = () => {
       <div className="px-8 py-8">
         <Breadcrumbs />
         <h1 className="text-2xl font-bold mb-4 mt-4">Dashboard</h1>
-        <div className="flex flex-col items-center gap-y-6 mt-8">
-          {" "}
-          <div className="grid grid-cols-7 gap-8">
-            {menuItems.slice(0, 9).map((item, index) => (
-              <MenuItem
-                key={index}
-                name={item.name}
-                icon={item.icon}
-                onClick={() => handleMenuItemClick(item.name)}
-              />
-            ))}
-          </div>
-        </div>{" "}
+      </div>
+      <div className="flex flex-col items-center gap-y-6">
+        <div className="grid grid-cols-7 gap-8">
+          {menuItems.slice(0, 7).map((item, index) => (
+            <MenuItem
+              key={index}
+              name={item.name}
+              icon={item.icon}
+              onClick={() => handleMenuItemClick(item.name.toString())}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
