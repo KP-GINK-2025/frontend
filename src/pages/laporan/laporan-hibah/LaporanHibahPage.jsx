562001413c9606cd5cc29d2d0b36d210f2a7bef7
yutaptra <yusriaftaputra.ya@gmail.com>
2025-07-04 12:04:45 +0700

    feat: implement Laporan Rekonsiliasi, Hibah, and LRA VS BM section subpages

---
+++ src/pages/laporan/laporan-hibah/LaporanHibahPage.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Layers } from "lucide-react";
+import Navbar from "../../../components/Navbar";
+import MenuItem from "../../../components/MenuItem";
+import useMenuNavigation from "../../../hooks/useMenuNavigation";
+import Breadcrumbs from "../../../components/Breadcrumbs";
+
+const menuItems = [
+  { name: "Hibah per UPB", icon: Layers },
+  { name: "Rekapitulasi Hibah", icon: Layers },
+  { name: "Rincian Hibah", icon: Layers },
+  { name: "Rekap Hibah", icon: Layers },
+];
+
+const LaporanHibahPage = () => {
+  const { handleMenuItemClick } = useMenuNavigation();
+
+  return (
+    <div className="min-h-screen bg-[#f7f7f7]">
+      {/* Header */}
+      <Navbar />
+
+      {/* Content */}
+      <div className="px-8 py-8">
+        <Breadcrumbs />
+        <h1 className="text-2xl font-bold mb-4 mt-4">Hibah</h1>
+      </div>
+      <div className="flex flex-col items-center gap-y-6">
+        <div className="grid grid-cols-3 gap-8">
+          {menuItems.slice(0, 3).map((item, index) => (
+            <MenuItem
+              key={index}
+              name={item.name}
+              icon={item.icon}
+              onClick={() => handleMenuItemClick(item.name.toString())}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LaporanHibahPage;
---
+++ src/pages/laporan/lra-vs-bm/LraVsBmPage.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Layers } from "lucide-react";
+import Navbar from "../../../components/Navbar";
+import MenuItem from "../../../components/MenuItem";
+import useMenuNavigation from "../../../hooks/useMenuNavigation";
+import Breadcrumbs from "../../../components/Breadcrumbs";
+
+const menuItems = [
+  { name: "LRA VS BM UPB", icon: Layers },
+  { name: "LRA VS BM SKPD", icon: Layers },
+];
+
+const LraVsBmPage = () => {
+  const { handleMenuItemClick } = useMenuNavigation();
+
+  return (
+    <div className="min-h-screen bg-[#f7f7f7]">
+      {/* Header */}
+      <Navbar />
+
+      {/* Content */}
+      <div className="px-8 py-8">
+        <Breadcrumbs />
+        <h1 className="text-2xl font-bold mb-4 mt-4">LRA VS BM</h1>
+      </div>
+      <div className="flex flex-col items-center gap-y-6">
+        <div className="grid grid-cols-2 gap-8 justify-items-center">
+          {menuItems.slice(0, 3).map((item, index) => (
+            <MenuItem
+              key={index}
+              name={item.name}
+              icon={item.icon}
+              onClick={() => handleMenuItemClick(item.name.toString())}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LraVsBmPage;
